993df2dced963229d3450d0559ae397f231217fa
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-09-28 18:06:37 -0300

    Added basic logic of character.

--- Character.lua
+++ Character.lua
@@ -3,4 +3,35 @@ posX = 0
 posY = 0
 velY = 0
 isJumping = false
+isFalling = false
 isOnGround = true
+
+function CharacterJump()
+	if(love.keyboard.isDown(up))then
+		if(not isJumping and isOnGround)then
+			isJumping = true
+			isOnGround = false
+			velY = 5
+		end
+	end
+end
+
+function CharacterFall()
+	if(isJumping and not isFalling)then
+		velY = -5
+	end
+	if(isFalling and isOnGround)then
+		isFalling = false
+		velY = 0
+	end
+end
+
+function UpdatePosition()
+	if(posY >= 30)then
+		isFalling = true
+	end
+	if(posY == 0)then
+		isOnGround = true
+	end
+	posY += velY
+end
